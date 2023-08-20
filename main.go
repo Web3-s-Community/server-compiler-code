3e205d46d1400f2ca5adf8616c66de701bad193f
Do Minh Phong <dominhphong306@gmail.com>
2023-08-20 20:56:15 +0700

    chore: add cors middleware

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
-   	ginSwagger "github.com/swaggo/gin-swagger"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	docs "server-compiler-code/docs"
 	// "server-compiler-code/models"
@@ -18,6 +18,7 @@ import (
 //	@BasePath	/api/v1
 
 // PingExample godoc
+//
 //	@Summary	ping example
 //	@Schemes
 //	@Description	do ping
@@ -26,35 +27,51 @@ import (
 //	@Produce		json
 //	@Success		200	{string}	Helloworld
 //	@Router			/example/helloworld [get]
-func Helloworld(g *gin.Context)  {
-	g.JSON(http.StatusOK,"helloworld")
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "helloworld")
 }
 
-//	@title		Your Gin API
-//	@version	1.0
-//	@host		localhost:8080
-//	@BasePath	/
+// @title		Your Gin API
+// @version	1.0
+// @host		localhost:8080
+// @BasePath	/
 func main() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	v1 := r.Group("/api/v1")
 	{
-	   eg := v1.Group("/example")
-	   {
-		  eg.GET("/helloworld",Helloworld)
-	   }
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", Helloworld)
+		}
 
-	   v1.POST("/execute-code", handlers.ExecuteCodeHandler)
-	   v1.POST("/execute-code-test", handlers.ExecuteCodeTestHandler)
-	   v1.POST("/format-code", handlers.FormatCodeHandler)
-	   v1.POST("/compiler-code", handlers.CompilerCodeHandler)
+		v1.POST("/execute-code", handlers.ExecuteCodeHandler)
+		v1.POST("/execute-code-test", handlers.ExecuteCodeTestHandler)
+		v1.POST("/format-code", handlers.FormatCodeHandler)
+		v1.POST("/compiler-code", handlers.CompilerCodeHandler)
 	}
 
 	// r.POST("/writeToFile", writeToFileHandler)
-
 
 	// Serve Swagger UI
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// Run the server on port 8080
 	r.Run(":8080")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
